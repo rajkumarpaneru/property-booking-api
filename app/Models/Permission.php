881a432ccb577afeae362c84908c9d0c71603277
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-04-28 22:01:27 +0545

    define role-permission relationships

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -10,4 +10,9 @@ class Permission extends Model
     use HasFactory;
 
     protected $fillable = ['name'];
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
 }
