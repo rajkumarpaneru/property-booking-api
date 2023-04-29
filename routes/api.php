5c270330620be9a8d6d595fe06d2bd0a56091c4a
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-04-29 11:28:33 +0545

    endpoint to list booking for user

--- routes/api.php
+++ routes/api.php
@@ -24,4 +24,7 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('owner/properties',
         [\App\Http\Controllers\Owner\PropertyController::class, 'index']);
+
+    Route::get('user/bookings',
+        [\App\Http\Controllers\User\BookingController::class, 'index']);
 });
