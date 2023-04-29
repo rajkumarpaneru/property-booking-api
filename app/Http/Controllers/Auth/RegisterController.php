a6696812095080a0643c4946ea9dfafddd535766
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-04-29 11:12:56 +0545

    add constants for roles

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -17,7 +18,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'confirmed', Password::defaults()],
-            'role_id' => ['required', Rule::in(2, 3)],
+            'role_id' => ['required', Rule::in(Role::ROLE_OWNER, Role::ROLE_USER)],
         ]);
 
         $user = User::create([
