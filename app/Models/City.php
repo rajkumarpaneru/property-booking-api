464d463668d3e3f242614a7aad3a34b301ea8dd9
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-04-30 20:58:51 +0545

    model and migration for city

---
+++ app/Models/City.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Models\Country;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class City extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['country_id', 'name', 'lat', 'long'];
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
+}
