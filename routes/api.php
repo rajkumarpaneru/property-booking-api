2723e94e473905d2085cc27df7bfd7a44c4a3ded
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-05-02 19:16:20 +0545

    endpoint to store property

--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,9 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('owner/properties',
         [\App\Http\Controllers\Owner\PropertyController::class, 'index']);
 
+    Route::post('owner/properties',
+        [\App\Http\Controllers\Owner\PropertyController::class, 'store']);
+
     Route::get('user/bookings',
         [\App\Http\Controllers\User\BookingController::class, 'index']);
 });
