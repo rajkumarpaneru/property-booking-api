b08575a04565210ae516fd5e4481e81cbf387358
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-05-02 19:09:04 +0545

    update database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,15 +6,14 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
         $this->call(RoleSeeder::class);
         $this->call(AdminUserSeeder::class);
+        $this->call(PermissionSeeder::class);
+
+        $this->call(CountrySeeder::class);
+        $this->call(CitySeeder::class);
+        $this->call(GeoobjectSeeder::class);
     }
 }
