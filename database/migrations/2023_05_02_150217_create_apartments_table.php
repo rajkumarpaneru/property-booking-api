9ac7c2d7287753367b0c7c2288df306bcad10363
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-05-02 20:48:49 +0545

    model, migration and factory for apartments

---
+++ database/migrations/2023_05_02_150217_create_apartments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('property_id')->constrained();
+            $table->string('name');
+            $table->unsignedInteger('capacity_adults');
+            $table->unsignedInteger('capacity_children');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartments');
+    }
+}
