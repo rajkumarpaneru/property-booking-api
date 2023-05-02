307d20996b127beac7597df2646494c5b5bf92de
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-05-02 19:12:50 +0545

    add property observer to convert address into coordinates

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Observers\PropertyObserver;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -22,5 +23,12 @@ class Property extends Model
     public function city()
     {
         return $this->belongsTo(City::class);
+    }
+
+    public static function booted()
+    {
+        parent::booted();
+
+        self::observe(PropertyObserver::class);
     }
 }
