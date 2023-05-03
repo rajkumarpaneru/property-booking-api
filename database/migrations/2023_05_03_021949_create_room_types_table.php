2d8482551db64561d660e6bb2b5429177643caf8
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-05-03 08:05:59 +0545

    migration and modal for room_type

---
+++ database/migrations/2023_05_03_021949_create_room_types_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\RoomType;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoomTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('room_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        RoomType::create(['name' => 'Bedroom']);
+        RoomType::create(['name' => 'Living room']);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('room_types');
+    }
+}
