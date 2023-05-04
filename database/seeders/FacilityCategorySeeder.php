ab9151d08871b961b747c49cacb8a2d6fa8e522c
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-05-04 21:57:32 +0545

    add model, migration and seeder for facility category

---
+++ database/seeders/FacilityCategorySeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\FacilityCategory;
+use Illuminate\Database\Seeder;
+
+class FacilityCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        FacilityCategory::create(['name' => 'Bedroom']);
+        FacilityCategory::create(['name' => 'Kitchen']);
+        FacilityCategory::create(['name' => 'Bathroom']);
+        FacilityCategory::create(['name' => 'Room Amenities']);
+        FacilityCategory::create(['name' => 'General']);
+        FacilityCategory::create(['name' => 'Media & Technology']);
+    }
+}
