7a62e6240c7798336c43e2a8f01b8c4166d4de5b
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-05-07 16:01:51 +0545

    show photos in search and property detail

--- app/Http/Controllers/User/PropertySearchController.php
+++ app/Http/Controllers/User/PropertySearchController.php
@@ -16,7 +16,10 @@ class PropertySearchController extends Controller
             ->with([
                 'city',
                 'apartments.apartment_type',
-                'apartments.rooms.beds.bed_type'
+                'apartments.rooms.beds.bed_type',
+                'facilities',
+                // We add only this eager loading by position here
+                'media' => fn($query) => $query->orderBy('position'),
             ])
             ->when($request->city, function ($query) use ($request) {
                 $query->where('city_id', $request->city);
