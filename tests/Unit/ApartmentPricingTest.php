7f5772ec9302068dbdfd2fc702f13989d9b88ddf
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-05-08 21:23:41 +0545

    unit test for apartment pricing

---
+++ tests/Unit/ApartmentPricingTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ApartmentPricingTest extends TestCase
+{
+    public function test_pricing_for_single_price(): void
+    {
+        $prices = collect([
+            [
+                'start_date' => '2023-05-01',
+                'end_date' => '2030-05-01',
+                'price' => 100
+            ]
+        ]);
+
+        $priceForOneDay = $this->pricingService->calculateApartmentPriceForDates(
+            $prices,
+            '2023-05-11',
+            '2023-05-11'
+        );
+
+        $this->assertEquals(100, $priceForOneDay);
+    }
+}
